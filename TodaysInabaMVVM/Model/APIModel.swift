8a7adfb0a7d65d6162bad5a4ec615539bc1e7df6
深瀬 貴将 <fukase@arsaga.jp>
2020-10-08 01:14:37 +0900

    APIのメソッドのモデルファイルを作成　GETメソッド追加

---
+++ TodaysInabaMVVM/Model/APIModel.swift
@@ -0,0 +1,28 @@
+//
+//  APIModel.swift
+//  TodaysInabaMVVM
+//
+//  Created by 深瀬 貴将 on 2020/10/08.
+//
+
+import Foundation
+import Moya
+import RxSwift
+
+final class APIModel {
+    
+    let provider = MoyaProvider<API>()
+    
+    static func getTodaysInabaImages() -> Observable<GoogleData> {
+        return provider.rx.request(
+            .CustomSearch(
+                query: "稲葉浩志" + ["かっこいい", "かわいい", "眼鏡", "へそ", "97年"].randomElement()!,
+                startIndex: Int.random(in: 1...10)))
+            .map { response in
+                
+                try! JSONDecoder().decode(GoogleData.self, from: response.data)
+                
+            }.asObservable()
+    }
+    
+}
