37dc2f21b6dda707369dce684b7d2245b7d747e2
深瀬 貴将 <fukase@arsaga.jp>
2020-10-10 23:42:42 +0900

    結果画面をoverCurrentContextにして背景を透かし、影などを追加

---
+++ TodaysInabaMVVM/Extension+Storyboard.swift
@@ -0,0 +1,68 @@
+//
+//  Extension+Storyboard.swift
+//  ARMeasureTest
+//
+//  Created by 深瀬 貴将 on 2020/08/08.
+//  Copyright © 2020 深瀬 貴将. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    @IBInspectable var cornerRadius: Double {
+        get {
+            return Double(self.layer.cornerRadius)
+        }set {
+            self.layer.cornerRadius = CGFloat(newValue)
+        }
+    }
+    @IBInspectable var borderWidth: Double {
+        get {
+            return Double(self.layer.borderWidth)
+        }
+        set {
+            self.layer.borderWidth = CGFloat(newValue)
+        }
+    }
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            return UIColor(cgColor: self.layer.borderColor!)
+        }
+        set {
+            self.layer.borderColor = newValue?.cgColor
+        }
+    }
+    @IBInspectable var shadowColor: UIColor? {
+        get {
+            return UIColor(cgColor: self.layer.shadowColor!)
+        }
+        set {
+            self.layer.shadowColor = newValue?.cgColor
+        }
+    }
+    @IBInspectable var shadowOpacity: Float {
+        get {
+            return self.layer.shadowOpacity
+        }
+        set {
+            self.layer.shadowOpacity = newValue
+        }
+    }
+    @IBInspectable var shadowRadius: CGFloat {
+        get {
+            return self.layer.shadowRadius
+        }
+        set {
+            self.layer.shadowRadius = newValue
+        }
+    }
+    @IBInspectable var shadowOffset: CGSize {
+        get {
+            return self.layer.shadowOffset
+        }
+        set {
+            self.layer.shadowOffset = newValue
+        }
+    }
+}
