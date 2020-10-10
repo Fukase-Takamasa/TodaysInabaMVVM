72a7adaf98940bf96cfa6936f3325d72f542e596
深瀬 貴将 <fukase@arsaga.jp>
2020-10-10 16:36:33 +0900

    履歴のSwiftUI画面作成

--- TodaysInabaMVVM/View/History/HistoryView.swift
+++ TodaysInabaMVVM/View/History/HistoryView.swift
@@ -9,8 +9,25 @@ import SwiftUI
 import struct Kingfisher.KFImage
 
 struct HistoryView: View {
+    
+    @State var imageUrlArr: [String] = []
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { bodyView in
+            ScrollView(.vertical) {
+                LazyVGrid(columns: Array(repeating: GridItem(), count: 2), spacing: 0) {
+                    ForEach(0..<self.imageUrlArr.count, id: \.self) { index in
+                        KFImage(URL(string: self.imageUrlArr[index]))
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: bodyView.size.width / 2, height: bodyView.size.width / 2)
+                    }
+                }
+            }
+            .onAppear {
+                self.imageUrlArr = UserDefaultsModel.getUrlArr()
+            }
+        }
     }
 }
 
